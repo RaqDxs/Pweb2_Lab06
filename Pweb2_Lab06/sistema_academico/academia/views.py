20dd7db3ce367808e6c813c8e4a57e0e08ed16ba
aquispearr <aquispearr@unsa.edu.com>
2024-05-29 15:50:44 -0500

    Views: Creacion de vistas para manejar los formularios

--- Pweb2_Lab06/sistema_academico/academia/views.py
+++ Pweb2_Lab06/sistema_academico/academia/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+# academia/views.py
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm, CursoForm, NotaForm
+from .models import NotasAlumnosPorCurso
 
 # Create your views here.
+
+def crear_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_alumnos')
+    else:
+        form = AlumnoForm()
+    return render(request, 'academia/crear_alumno.html', {'form': form})
+
+def crear_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'academia/crear_curso.html', {'form': form})
+
+def ingresar_nota(request):
+    if request.method == 'POST':
+        form = NotaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_notas')
+    else:
+        form = NotaForm()
+    return render(request, 'academia/ingresar_nota.html', {'form': form})
+
+def lista_alumnos(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'academia/lista_alumnos.html', {'alumnos': alumnos})
+
+def lista_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'academia/lista_cursos.html', {'cursos': cursos})
+
+def lista_notas(request):
+    notas = NotasAlumnosPorCurso.objects.all()
+    return render(request, 'academia/lista_notas.html', {'notas': notas})
