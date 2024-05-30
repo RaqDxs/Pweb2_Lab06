887b861f2cf3b27a9748d5e1c2314be8ab62d93e
aquispearr <aquispearr@unsa.edu.com>
2024-05-30 00:51:34 -0500

    Agregamos estilos

--- Pweb2_Lab06/sistema_academico/sistema_academico/urls.py
+++ Pweb2_Lab06/sistema_academico/sistema_academico/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('academia/', include('academia.urls')),
 ]
+
+# Configuración de archivos estáticos en modo de desarrollo
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
