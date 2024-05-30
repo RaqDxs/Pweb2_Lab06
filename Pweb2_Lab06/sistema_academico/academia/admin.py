4980635fb94e3b7139e3064fa27b8d7752978b5e
aquispearr <aquispearr@unsa.edu.com>
2024-05-29 22:52:53 -0500

    Creacion de las plantillas

--- Pweb2_Lab06/sistema_academico/academia/admin.py
+++ Pweb2_Lab06/sistema_academico/academia/admin.py
@@ -1,12 +1,1 @@
 from django.contrib import admin
-from .models import Alumno, Curso, NotasAlumnosPorCurso
-
-class AlumnoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'apellido', 'email', 'matricula')
-    search_fields = ('nombre', 'apellido', 'email', 'matricula')
-
-admin.site.register(Alumno, AlumnoAdmin)
-admin.site.register(Curso)
-admin.site.register(NotasAlumnosPorCurso)
-
-# Register your models here.
