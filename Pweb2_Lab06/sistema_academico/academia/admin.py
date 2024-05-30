7ac23e4874c94a6e1945be7f65d10bc79f622e29
PaulCari <pcaril@unsa.edu.pe>
2024-05-29 22:36:30 -0500

    Mejorando admin.py

--- Pweb2_Lab06/sistema_academico/academia/admin.py
+++ Pweb2_Lab06/sistema_academico/academia/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from .models import Alumno, Curso, NotasAlumnosPorCurso
 
-from .models.Alumno import Alumno
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'email', 'matricula')
+    search_fields = ('nombre', 'apellido', 'email', 'matricula')
 
-admin.site.register(Alumno)
+admin.site.register(Alumno, AlumnoAdmin)
+admin.site.register(Curso)
+admin.site.register(NotasAlumnosPorCurso)
+
 # Register your models here.
